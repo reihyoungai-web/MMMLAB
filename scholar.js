f1a926b801f941bc1179258c223baa29ad74ffb9
reihyoungai-web <reihyoungai@gmail.com>
2026-03-10 00:44:11 +0900

    Add files via upload

--- scholar.js
+++ scholar.js
@@ -2,7 +2,7 @@ async function loadPublications(){
 
   const scholarID = "BimZFYAAAAAJ&hl=ko";
   
-  const url = "https://api.allorigins.win/raw?url=https://scholar.googleusercontent.com/citations?user="+scholarID;
+  const url = "https://api.allorigins.win/raw?url=https://scholar.googleusercontent.com/citations?user=BimZFYAAAAAJ&hl=ko";
   
   const res = await fetch(url);
   const data = await res.text();
